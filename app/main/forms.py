fe99ddff8571ecf8039bd3442c8a3bf321dfab56
zuiryuu <jcslmu@gmail.com>
2017-12-30 08:50:03 +0800

    comments and comments moderation

--- app/main/forms.py
+++ app/main/forms.py
@@ -45,3 +45,7 @@ class EditProfileAdminForm(FlaskForm):
 class PostForm(FlaskForm):
     body = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    body = StringField("Enter your comment", validators=[Required()])
+    submit = SubmitField('Submit')
