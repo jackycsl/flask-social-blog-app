f0560ac72034b42ee2204702fc45ea191f69c33b
zuiryuu <jcslmu@gmail.com>
2017-12-30 19:20:58 +0800

    API

--- app/__init__.py
+++ app/__init__.py
@@ -34,4 +34,7 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    from .api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
     return app
